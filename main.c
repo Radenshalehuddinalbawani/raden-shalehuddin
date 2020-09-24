4272f1477d93dc94b87b979ecb55e1f5ddc69be5
Radenshalehuddinalbawani <fanimaho1999@gmail.com>
2020-09-24 11:09:48 +0700

    Init

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	// Menampilkan Nilai bilangan integer
+	printf("\nNo                : %d", 134); 
+	// Menampilkan Nilai bilangan real
+	printf("\nNilai             : %f", 10.65);
+	// Menampilkan string
+	printf("\nNama              : %s", "Bang Toyib");
+	// Menampilkan karakter
+	printf("\nNilai Anda adalah : %c", 'A');
+}
